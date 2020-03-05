b37694a94ea0ba92efe9823f58f5fc96d2770b19
winnie3269 <wang.winnie26@gmail.com>
2020-03-05 13:11:18 -0800

    added constraints and class file for the logout button

---
+++ adl/Profile.swift
@@ -0,0 +1,17 @@
+//
+//  Profile.swift
+//  adl
+//
+//  Created by Winnie Wang on 3/5/20.
+//  Copyright © 2020 Jack Sheridan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Profile : UIViewController {
+    
+    
+    @IBOutlet weak var logOutBtn: UIButton!
+    
+}
